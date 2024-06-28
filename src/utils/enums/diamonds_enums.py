b439e5942945ca10c59db5693388cc8db4afb809
paolaagrassi <paola.grassi2014@gmail.com>
2024-06-28 10:55:23 -0300

    feat: create enums for color, clarity and cut

---
+++ src/utils/enums/diamonds_enums.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+class DiamondColorEnum(str,Enum):
+    d = "D"
+    e = "E"
+    f = "F"
+    g = "G"
+    h = "H"
+    i = "I"
+    j = "J"
+
+class DiamondClarityEnum(str, Enum):
+    iF = "IF"
+    vvs1 = "VVS1"
+    vvs2 = "VVS2"
+    vs1 = "VS1"
+    vs2 = "VS2"
+    si1 = "SI1"
+    si2 = "SI2"
+    i1 = "I1"
+
+class DiamondCutEnum(str, Enum):
+    fair = "Fair"
+    good = "Good"
+    very_good = "Very Good"
+    ideal = "Ideal"
+    premium = "Premium"
