9f0b2ada59ae382f428e0c69e8a8a3e20027f135
paolaagrassi <paola.grassi2014@gmail.com>
2024-06-29 22:19:58 -0300

    feat: add configs for testing apis with db connection

--- tests/e2e/test_diamonds_router.py
+++ tests/e2e/test_diamonds_router.py
@@ -1,19 +1,14 @@
-from fastapi.testclient import TestClient
-from pytest_mock import MockerFixture
-from src.main import app
 import pytest
-import pandas as pd
-
-client = TestClient(app)
-
 
 class TestPostPredictedDiamondPrice:
-    def test_post_predicted_diamond_price(self, mocker):
+    @pytest.mark.db
+    def test_post_predicted_diamond_price(self, mocker, client):
         mocker.patch(
             "src.services.diamond_service.predict_diamond_price",
             return_value="The predicted value for the diamond is: $5358.2705078125",
         )
 
+        mocker.patch("src.services.api_requests_service.save_api_requests_to_database")
         data = {
             "carat": 1,
             "cut": "Fair",
@@ -64,7 +59,7 @@ class TestPostPredictedDiamondPrice:
             ),
         ],
     )
-    def test_raises_400_when_body_data_has_0_values(self, body_data):
+    def test_raises_400_when_body_data_has_0_values(self, body_data, client):
 
         response = client.post("/diamond/predict-price", json=body_data)
 
@@ -72,14 +67,15 @@ class TestPostPredictedDiamondPrice:
 
 
 class TestPostSearchDiamondByFeaturesAndSimilarWeight:
-    def test_post_search_diamond_by_features_and_similar_weight(self):
-        data = {"carat": 1, "cut": "Ideal", "color": "H", "clarity": "SI1"}
+    def test_post_search_diamond_by_features_and_similar_weight(self, client):
+        data = {"carat": 0.96, "cut": "Ideal", "color": "H", "clarity": "SI2"}
 
         response = client.post("/diamond/search", json=data)
 
         assert response is not None
+        assert response.status_code == 200
 
-    def test_raises_400_when_there_is_no_results(self):
+    def test_raises_400_when_there_is_no_results(self, client):
         data = {"carat": 1, "cut": "Fair", "color": "D", "clarity": "IF"}
 
         response = client.post("/diamond/search", json=data)
@@ -90,7 +86,7 @@ class TestPostSearchDiamondByFeaturesAndSimilarWeight:
             == "It was not found diamonds for the chosen features."
         )
 
-    def test_raises_400_when_carat_is_zero(self):
+    def test_raises_400_when_carat_is_zero(self, client):
         data = {"carat": 0, "cut": "Fair", "color": "D", "clarity": "IF"}
 
         response = client.post("/diamond/search", json=data)
